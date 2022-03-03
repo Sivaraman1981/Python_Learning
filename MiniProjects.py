270d5d61b680610aac749715c6b75a5712fb017f
Sivaraman1981 <98298387+Sivaraman1981@users.noreply.github.com>
2022-03-02 21:41:39 -0500

    Add files via upload

--- MiniProjects.py
+++ MiniProjects.py
@@ -11,52 +11,61 @@
 #USER wins the game !!! Congrats !!!!
 
 import random
-
-NoofGames = int(input("Enter the number of games do you want to play::"))
-
-Li1 = ["rock","paper","scissor"]
+import sys
 
 v_tie=0
 v_user=0
 v_sys=0
 
+
+try:
+    NoofGames = int(input("Enter the number of games do you want to play::"))
+except Exception as error:
+    print("Enter the valid values to play the Games::")
+    sys.exit()
+    
+Li1 = ["rock","paper","scissor"]
+
 for i in range(NoofGames):
 
-
-    User_In = input("Enter the values of rock paper scissor:::")
-    print("User Input::",User_In)
-    sys_ran = random.choice(Li1)
-    print("System Input::",sys_ran)
-    
-    if User_In =='rock' and (User_In==sys_ran):
-        v_tie=v_tie+1
-        print("The game is tie")
-    elif User_In =='paper' and (User_In==sys_ran):
-        v_tie=v_tie+1
-        print("The game is tie")
-    elif User_In =='scissor' and (User_In==sys_ran):
-        v_tie=v_tie+1
-        print("The game is tie")
-    elif User_In =='rock' and sys_ran =='scissor':
-        v_user=v_user+1
-        print("User won the game")
-    elif sys_ran =='rock' and User_In =='scissor':
-        v_sys=v_sys+1
-        print("System won the game")
-    elif User_In =='paper' and sys_ran =='scissor':
-        v_sys=v_sys+1
-        print("System won the game")
-    elif sys_ran =='paper' and User_In =='scissor':
-        v_user=v_user+1
-        print("User won the game")
-    elif User_In =='rock' and sys_ran =='paper':
-        v_sys=v_sys+1
-        print("System won the game")
-    elif sys_ran =='rock' and User_In =='paper':
-        v_user=v_user+1
-        print("User won the game")
-    else:
-        print("Invalid Input given by User")
+        User_In = input("Enter the values of rock paper scissor:::")          
+          
+        print("User Input::",User_In)
+        sys_ran = random.choice(Li1)
+        print("System Input::",sys_ran)
+            
+        if User_In =='rock' and (User_In==sys_ran):
+            v_tie=v_tie+1
+            print("The game is tie")
+        elif User_In =='paper' and (User_In==sys_ran):
+            v_tie=v_tie+1
+            print("The game is tie")
+        elif User_In =='scissor' and (User_In==sys_ran):
+            v_tie=v_tie+1
+            print("The game is tie")
+        elif User_In =='rock' and sys_ran =='scissor':
+            v_user=v_user+1
+            print("User won the game")
+        elif sys_ran =='rock' and User_In =='scissor':
+            v_sys=v_sys+1
+            print("System won the game")
+        elif User_In =='paper' and sys_ran =='scissor':
+            v_sys=v_sys+1
+            print("System won the game")
+        elif sys_ran =='paper' and User_In =='scissor':
+            v_user=v_user+1
+            print("User won the game")
+        elif User_In =='rock' and sys_ran =='paper':
+            v_sys=v_sys+1
+            print("System won the game")
+        elif sys_ran =='rock' and User_In =='paper':
+            v_user=v_user+1
+            print("User won the game")
+        elif User_In not in('rock','paper','scissor') and sys_ran in('rock','paper','scissor'):
+            v_sys=v_sys+1
+            print("System won the game")
+        else:
+            print("Invalid Input given by User")
 
 print("\nTotal No of Games::",NoofGames)
 print("System points::",v_sys)
@@ -71,45 +80,3 @@ elif v_user == v_sys and (v_user>0 and v_sys>0):
     print("\nUSER & SYSTEM both are wins the game !!! Congrats !!!!")
 elif v_user == 0 and v_sys == 0:
     print("\nUSER & SYSTEM both are not wins the game !!! Congrats !!!!")
-
-Output:
-
-========================================== RESTART: C:\Users\sivar\AppData\Local\Programs\Python\Python310\2.py =========================================
-Enter the number of games do you want to play::3
-Enter the values of rock paper scissor:::rock
-User Input:: rock
-System Input:: paper
-System won the game
-Enter the values of rock paper scissor:::paper
-User Input:: paper
-System Input:: rock
-User won the game
-Enter the values of rock paper scissor:::scissor
-User Input:: scissor
-System Input:: scissor
-The game is tie
-
-Total No of Games:: 3
-System points:: 1
-User points:: 1
-Total tie points:: 1
-
-USER & SYSTEM both are wins the game !!! Congrats !!!!
-
-========================================== RESTART: C:\Users\sivar\AppData\Local\Programs\Python\Python310\2.py =========================================
-Enter the number of games do you want to play::2
-Enter the values of rock paper scissor:::rrrr
-User Input:: rrrr
-System Input:: paper
-Invalid Input given by User
-Enter the values of rock paper scissor:::rock
-User Input:: rock
-System Input:: rock
-The game is tie
-
-Total No of Games:: 2
-System points:: 0
-User points:: 0
-Total tie points:: 1
-
-USER & SYSTEM both are not wins the game !!! Congrats !!!!
